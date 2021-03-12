14a63e07d49950bb9762ae5a62800ecafb4aaea5
Tianqi <tqzhang@ucdavis.edu>
2021-03-12 15:30:10 -0800

    Moved wavedump to current directory

--- writedata.sh
+++ writedata.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 read -p 'Pressure: ' prevar
 read -p 'Run number: ' rint
-cp /home/samd/Downloads/wavedump-3.10.0/wave0.txt /home/samd/Downloads/SAMDdata/r${rint}_pres${prevar}wave0.txt
-cp /home/samd/Downloads/wavedump-3.10.0/wave1.txt /home/samd/Downloads/SAMDdata/r${rint}_pres${prevar}wave1.txt
+cp wave0.txt /home/samd/Downloads/SAMDdata/r${rint}_pres${prevar}wave0.txt
+cp wave1.txt /home/samd/Downloads/SAMDdata/r${rint}_pres${prevar}wave1.txt
